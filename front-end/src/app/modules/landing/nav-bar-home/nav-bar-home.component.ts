c9402c0bad30a19dc8a12caa8d3ed9fff01ea37c
Dilaksan <dilaksanraj510@gmail.com>
2023-11-02 01:37:52 +0100

    changes with nav color

--- front-end/src/app/modules/landing/nav-bar-home/nav-bar-home.component.ts
+++ front-end/src/app/modules/landing/nav-bar-home/nav-bar-home.component.ts
@@ -1,27 +1,53 @@
-import { ChangeDetectionStrategy, Component, HostListener, OnInit, ViewEncapsulation } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import {
+    ChangeDetectionStrategy,
+    Component,
+    HostListener,
+    Inject,
+    OnInit,
+    ViewEncapsulation,
+} from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-nav-bar-home',
-  templateUrl: './nav-bar-home.component.html',
-  styleUrls: ['./nav-bar-home.component.scss'],
-  encapsulation  : ViewEncapsulation.None,
-    changeDetection: ChangeDetectionStrategy.OnPush
+    selector: 'app-nav-bar-home',
+    templateUrl: './nav-bar-home.component.html',
+    styleUrls: ['./nav-bar-home.component.scss'],
+    encapsulation: ViewEncapsulation.None,
+    changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class NavBarHomeComponent implements OnInit {
     header_variable = true;
-  constructor( private _router: Router) { }
+    constructor(
+        private _router: Router,
+        @Inject(DOCUMENT) private document: Document
+    ) {}
 
-  ngOnInit(): void {
-  }
+    @HostListener('window:scroll', [])
+    onWindowScroll() {
+        if (
+            document.body.scrollTop > 100 ||
+            document.documentElement.scrollTop > 100
+        ) {
+            console.log('scrolled');
 
-  goTo(fragment:any): void{
+            document.getElementById('subTitle').classList.add('scrolled');
+            this.document.getElementById('subTitle').classList.remove('header_change')
+        }
 
-    this._router.navigateByUrl('home#'+fragment);
+        else{
+            document.getElementById('subTitle').classList.add('header_change');
+            this.document.getElementById('subTitle').classList.remove('scrolled')
+        }
+    }
 
-  }
-  @HostListener('document.scroll')
-  scrollfunction(){
+    ngOnInit(): void {}
 
-  }
+    goTo(fragment: any): void {
+        this._router.navigateByUrl('home#' + fragment);
+    }
+    @HostListener('document.scroll')
+    onScroll(event: any) {
+        console.log(event);
+    }
 }
