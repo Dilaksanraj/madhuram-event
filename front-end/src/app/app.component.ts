0fb1ee85a5021bf3bfc2f740f8740bb41882ff01
Dilaksan <dilaksanraj510@gmail.com>
2023-11-21 00:20:25 +0100

    footer removed

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import * as AOS from 'aos';
 @Component({
     selector   : 'app-root',
@@ -10,6 +10,10 @@ export class AppComponent implements OnInit
     /**
      * Constructor
      */
+    isShow: boolean;
+  topPosToStartShowing = 100;
+
+
     constructor()
     {
     }
@@ -19,5 +23,30 @@ export class AppComponent implements OnInit
         AOS.init();
 
 
+      }
+
+      @HostListener('window:scroll')
+  checkScroll() {
+
+    // window의 scroll top
+    // Both window.pageYOffset and document.documentElement.scrollTop returns the same result in all the cases. window.pageYOffset is not supported below IE 9.
+
+    const scrollPosition = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+
+    console.log('[scroll]', scrollPosition);
+
+    if (scrollPosition >= this.topPosToStartShowing) {
+      this.isShow = true;
+    } else {
+      this.isShow = false;
+    }
+  }
+
+      gotoTop() {
+        window.scroll({
+          top: 0,
+          left: 0,
+          behavior: 'smooth'
+        });
       }
 }
