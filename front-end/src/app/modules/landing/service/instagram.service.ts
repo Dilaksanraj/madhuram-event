67cf1ddf45e655d09170c65d863b7606c2a27daa
Dilaksan <dilaksanraj510@gmail.com>
2023-12-04 14:10:45 +0100

    token updated

--- front-end/src/app/modules/landing/service/instagram.service.ts
+++ front-end/src/app/modules/landing/service/instagram.service.ts
@@ -6,13 +6,21 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class InstagramService {
-  private accessToken = `IGQWROSEdkOGdQNXV6NXprd0k2dE9EUWxDUlgyclhldUFFMXZAVbVVwWHhPREFQRkhhaGJRUm9OTndJdWhDTkwwWi1VUHJ6Y19GRV93VS1VcUJJcU9VMHhmSkxRcTZAEMmVXVEthN3NGOFZACdVo2YkV5MlV3VVdzaUUZD`
+
+  private accessToken = `IGQWRNNEc1WmZAUb096ZA2JJZAVhhU2tfSlZAmVUZAVeTZALRDZAHTFY0RGhzNkk2VWRMZAmt4MXF6LUh0bW5paVh0REQ0S0V0a05yU0J2MmlHX01zVmkxQ29XVWVLeE1mUEMyMVZAYZAm1RU1NiQnVFa0dZAR3lFN1d2ZAWxCb0kZD`
   private app_id = '1487706665418963'
   private app_secret = 'e28fd50b701e5fe150ea3dc1f5cc67ef'
   private apiUrl = `https://graph.instagram.com/me/media?fields=id,media_type,media_url,thumbnail_url,caption&
   client_id=${this.app_id}&
   client_secret=${this.app_secret}&
   access_token=${this.accessToken}`//`https://graph.instagram.com/me/media?fields=id,media_type,media_url,thumbnail_url,caption&access_token=${this.accessToken}`;
+
+
+  private accessTokenUrl = `https://graph.facebook.com/{graph-api-version}/oauth/access_token?
+  grant_type=fb_exchange_token&
+  client_id=${this.app_id}&
+  client_secret=${this.app_secret}&
+  fb_exchange_token=${this.accessToken}`
 
   constructor(private http: HttpClient) {}
 
