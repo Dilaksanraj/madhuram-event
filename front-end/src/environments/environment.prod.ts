8a697de76a833516e419321556a7ca18a68f3e72
Dilaksan <dilaksanraj510@gmail.com>
2023-12-08 14:46:16 +0100

    env updated

--- front-end/src/environments/environment.prod.ts
+++ front-end/src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-    production: true
+    production: true,
+    accessToken: 'IGQWRNMFQ1eFdzeFBJQngzeFZACRURGX0JhaVdqRVE2U0NjNHlVWVBva2djZAm05aHUzc3lvY0lGWTh0RWdtZAEhZARjctOFFhc3RsdDVwN083ekdvd2hPT0UwUHZAHVy0xMWd5YVFpeUtPQ2VOZA0hxUndKMVhOci13TEkZD'
 };
