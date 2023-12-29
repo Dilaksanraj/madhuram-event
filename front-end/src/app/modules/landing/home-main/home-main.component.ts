43f78906785b2155a6cc9e8e096362f463ff21c9
Dilaksan <dilaksanraj510@gmail.com>
2023-12-29 23:33:18 +0100

    banner chnaged

--- front-end/src/app/modules/landing/home-main/home-main.component.ts
+++ front-end/src/app/modules/landing/home-main/home-main.component.ts
@@ -1,72 +1,89 @@
 import { DOCUMENT } from '@angular/common';
-import { Component, HostListener, Inject, OnInit, ViewEncapsulation } from '@angular/core';
+import {
+    Component,
+    HostListener,
+    Inject,
+    OnInit,
+    ViewEncapsulation,
+} from '@angular/core';
 import { SlideInterface } from '../image-slider/interface/slide.interface';
 
 @Component({
-  selector: 'app-home-main',
-  templateUrl: './home-main.component.html',
-  styleUrls: ['./home-main.component.scss'],
-  encapsulation: ViewEncapsulation.None,
+    selector: 'app-home-main',
+    templateUrl: './home-main.component.html',
+    styleUrls: ['./home-main.component.scss'],
+    encapsulation: ViewEncapsulation.None,
 })
 export class HomeMainComponent implements OnInit {
+    roles: string[] = [
+        'Wedding',
+        'Birthday',
+        'Mahendi',
+        'Baby shower',
+        'Maternity',
+    ];
+    currentIndex = 0;
+    heading = 'We do';
 
-    textArray = ['wedding', 'birthday', 'reception'];
+    constructor(@Inject(DOCUMENT) private document: Document) {}
 
-
-roles: string[] = ['Wedding', 'Birthday', 'Mahendi', 'Baby shower', 'Maternity'];
-currentIndex = 0;
-heading = 'We do';
-
-  constructor(
-    @Inject(DOCUMENT) private document: Document
-  ) { }
-
-
-  @HostListener('window:scroll', [])
+    @HostListener('window:scroll', [])
     onWindowScroll() {
-
-        if(document.body.scrollTop > 10 ||
-            document.documentElement.scrollTop > 10){
-
-                // document.getElementById('mainImg').classList.add('bg-color');
-                // this.document.getElementById('mainImg').classList.remove('bg-luminosity');
-
-                // document.getElementById('btn-home').classList.add('bg-app');
-                // this.document.getElementById('mainImg').classList.remove('bg-black');
-
-
-        }
-
-        else{
+        if (
+            document.body.scrollTop > 10 ||
+            document.documentElement.scrollTop > 10
+        ) {
+            // document.getElementById('mainImg').classList.add('bg-color');
+            // this.document.getElementById('mainImg').classList.remove('bg-luminosity');
+            // document.getElementById('btn-home').classList.add('bg-app');
+            // this.document.getElementById('mainImg').classList.remove('bg-black');
+        } else {
             // document.getElementById('mainImg').classList.add('bg-luminosity');
             // this.document.getElementById('mainImg').classList.remove('bg-color')
-
             // document.getElementById('btn-home').classList.add('bg-black');
             // this.document.getElementById('btn-home').classList.remove('bg-app')
         }
     }
 
     slides: SlideInterface[] = [
-        { url: '../../../../assets/images/about-us/ramyahindi.jpg', title: 'beach' },
-        { url: '../../../../assets/images/about-us/ramya-white.jpg', title: 'boat' },
-        { url: '../../../../assets/images/about-us/ramya-anu.jpg', title: 'forest' },
-        { url: '../../../../assets/images/about-us/safari.jpeg', title: 'city' },
-        { url: '../../../../assets/images/about-us/ramya-mathu.jpg', title: 'city' },
-      ];
+        {
+            url: '../../../../assets/images/wall/banner1.png',
+            title: 'beach',
+        },
+        {
+            url: '../../../../assets/images/wall/banner2.png',
+            title: 'boat',
+        },
+    ];
 
-  ngOnInit(): void {
+    ngOnInit(): void {
+        setInterval(() => {
+            this.currentIndex = (this.currentIndex + 1) % this.roles.length;
+        }, 2000); // Change the word every 2 seconds (adjust as needed)
+    }
 
-    setInterval(() => {
-        this.currentIndex = (this.currentIndex + 1) % this.roles.length;
-      }, 2000); // Change the word every 2 seconds (adjust as needed)
-  }
+    onScroll(event: any) {
+        console.log(event);
+    }
 
-  onScroll(event: any) {
-    console.log(event);
-}
+    getCurrentRole(): string {
+        // this.updateRoleText(this.roles[this.currentIndex])
+        return this.roles[this.currentIndex];
+    }
 
-getCurrentRole(): string {
-    return this.roles[this.currentIndex];
-  }
+     updateRoleText(newRole) {
+        const roleText = document.getElementById('roleText');
+        roleText.style.opacity = '0'; // Fading out the text
+
+        setTimeout(function () {
+            roleText.textContent = newRole;
+            roleText.style.opacity = '0'; // Fading in the new text
+        }, 300); // Adjust the duration based on your transition time in CSS (300ms in this case)
+    }
+
+    // Example of how to use the function:
+    // Call updateRoleText() whenever getCurrentRole() updates the role text
+    // For example:
+    // updateRoleText("New Role Text");
 
 }
